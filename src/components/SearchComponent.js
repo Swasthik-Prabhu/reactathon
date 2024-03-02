6457430ca752b3d05f7bf7f7d8b86ac087166668
Swasthik-Prabhu <swasthikp03@gmail.com>
2024-03-02 16:16:30 +0530

    first commit

---
+++ src/components/SearchComponent.js
@@ -0,0 +1,37 @@
+//components/SearchComponent.js
+import React from 'react';
+
+ 
+function SearchComponent({ searchCourse, courseSearchUserFunction }) {
+    return (
+        <header className="App-header">
+            <h1 > Shopping Cart</h1>
+            <div>
+                <br>
+                </br>
+            
+            </div>
+            
+            <div className="search-bar">
+                
+                <input
+                    type="text"
+                    placeholder="Search "
+                    value={searchCourse}
+                    onChange={courseSearchUserFunction}
+                />
+               
+                
+            </div>
+            
+        
+
+            
+           
+            
+        </header>
+    );
+}
+
+ 
+export default SearchComponent;
